769b88a3651b128faaa6db502045d608a825954b
a15929081 <1435914470@qq.com>
2019-05-28 16:46:08 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div>
+<input type="text" />
+<button>提交</button>
+
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+export default{
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
 }
-</script>
+</script> yarn serve
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
